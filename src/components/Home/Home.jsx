a1b74abbbdbdda6d059834a7c4571b7ee3f70fd4
ors-reyes <ors2323@gmail.com>
2025-10-11 23:43:54 -0300

    home-v1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,18 @@
+import "./home.css"
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+      <div className="hero">
+        <div className="hero-Text">
+          <p>Un desarrollo residencial premium en Luque: seguridad, diseño y amenities pensados para una vida de confort.</p>
+          <div className="hero-button">
+            <button className="gold">Ver terrenos disponibles</button>
+            <button className="blue">Ver Ubicación</button>
+          </div>
+        </div>
+      </div>
+      <div className="relleno"></div>
+    </section>
   )
 }
 export default Home
