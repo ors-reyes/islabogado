a5013dfe01c77e4cbf3aa6634fb1a9323e8e7df7
ors-reyes <ors2323@gmail.com>
2025-10-15 05:51:27 -0300

    plano

---
+++ src/components/Terrenos/Planos/Planos.jsx
@@ -0,0 +1,233 @@
+import "./Planos.css"
+const Planos = () => {
+  return (
+    <svg viewBox="0 0 1400 700" className="planoSVG">
+        <defs>
+            <g id="l01"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">01</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">315m²</text>{/* Area */}
+            </g>
+            <g id="l02"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">02</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">299m²</text>{/* Area */}
+            </g>
+            <g id="l03"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">03</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">299m²</text>{/* Area */}
+            </g>
+            <g id="l04"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">04</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">299m²</text>{/* Area */}
+            </g>
+            <g id="l05"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">05</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">298m²</text>{/* Area */}
+            </g>
+            <g id="l06"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">06</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">298m²</text>{/* Area */}
+            </g>
+            <g id="l07"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">07</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">297m²</text>{/* Area */}
+            </g>
+            <g id="l08"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">08</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">297m²</text>{/* Area */}
+            </g>
+            <g id="l09"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">09</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">296m²</text>{/* Area */}
+            </g>
+            <g id="l10"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">10</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">255m²</text>{/* Area */}
+            </g>
+            <g id="l11"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">11</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">242m²</text>{/* Area */}
+            </g>
+            <g id="l12"><rect className="fnd"/><text className="textManzana" x="250" y="250">Manzana</text><text className="textManzana" x="250" y="370">Lote</text><circle className="circle"/>
+                <text className="textInput" x="292" y="310">1</text>{/* Manzana */}
+                <text className="textInput" x="320" y="370">12</text>{/* Lote */}
+                <text className="textInput" x="250" y="420">257m²</text>{/* Area */}
+            </g>
+        </defs>
+        <g className="Manzana01">
+            <polygon className="man01 l01" points="1226.1,77.4 1323.4,77.4 1320.6,39.2 1220.3,39.2 1220.4,39.8"/>
+            <use href="#l01" x="100" y="20" opacity="0" className="lot01"/>
+            <polygon className="man01 l02" points="1323.4,77.4 1226.1,77.4 1231.9,116.4 1326.2,116.4"/>
+            <use href="#l02" x="100" y="20" opacity="0" className="lot02"/>
+            <polygon className="man01 l03" points="1326.2,116.4 1231.9,116.4 1238.1,157.7 1329.1,157.7"/>
+            <use href="#l03" x="100" y="20" opacity="0" className="lot03"/>
+            <polygon className="man01 l04" points="1329.1,157.7 1238.1,157.7 1244.3,199.1 1332.1,199.1"/>
+            <use href="#l04" x="100" y="20" opacity="0" className="lot04"/>
+            <polygon className="man01 l05" points="1332.1,199.1 1244.3,199.1 1251.3,245.8 1335.4,245.8"/>
+            <use href="#l05" x="100" y="20" opacity="0" className="lot05"/>
+            <polygon className="man01 l06" points="1335.4,245.8 1251.3,245.8 1258.4,292.5 1338.8,292.5"/>
+            <use href="#l06" x="100" y="20" opacity="0" className="lot06"/>
+            <polygon className="man01 l07" points="1338.8,292.5 1258.4,292.5 1266.1,344.1 1342.5,344.1"/>
+            <use href="#l07" x="100" y="20" opacity="0" className="lot07"/>
+            <polygon className="man01 l08" points="1342.5,344.1 1266.1,344.1 1273.8,395.7 1346.2,395.7"/>
+            <use href="#l08" x="100" y="20" opacity="0" className="lot08"/>
+            <polygon className="man01 l09" points="1346.2,395.7 1273.8,395.7 1282.4,452.5 1350.3,452.5"/>
+            <use href="#l09" x="100" y="20" opacity="0" className="lot09"/>
+            <polygon className="man01 l10" points="1350.3,452.5 1282.4,452.5 1289.9,502.5 1353.9,502.5"/>
+            <use href="#l10" x="100" y="20" opacity="0" className="lot10"/>
+            <polygon className="man01 l11" points="1353.9,502.5 1289.9,502.5 1298.1,557.1 1357.8,557.1"/>
+            <use href="#l11" x="100" y="20" opacity="0" className="lot11"/>
+            <polygon className="man01 l12" points="1357.8,557.1 1298.1,557.1 1310.2,637.8 1363.6,637.8"/>
+            <use href="#l12" x="100" y="20" opacity="0" className="lot12"/>
+        </g>
+
+
+        <g className="opacity50">
+        <polygon className="st0" points="1001.8,550.2 1049.5,551.7 1065.3,681.6 1017.7,681.6 	"/>
+        <polygon className="st1" points="1049.5,551.7 1100.2,553.3 1116.7,681.6 1066.3,681.6 	"/>
+        <polygon className="st2" points="1101,553.3 1151.7,554.8 1168,681.6 1117.6,681.6 	"/>
+        <polygon className="st3" points="1152.2,554.8 1202.9,556.4 1219,681.6 1168.6,681.6 	"/>
+        <polygon className="st4" points="1017.6,681.6 1001.6,550.3 950.8,548.8 967.1,681.6 	"/>
+        <polygon className="st5" points="1203.3,556.4 1219.2,680.6 1267.8,680.2 1248.3,557.7 	"/>
+        </g>
+        
+        <g className="opacity50">
+        <polygon className="st18" points="192.7,529.1 229.2,530 231.9,689.1 184.1,690.1 180.8,540.6 	"/>
+        <polygon className="st19" points="229.2,530 276.9,531.2 279.7,688.7 231.9,689.1 	"/>
+        <polygon className="st20" points="276.9,531.2 324.5,532.4 331,688.3 279.7,688.7 	"/>
+        <polygon className="st21" points="324.5,532.4 372,533.6 378.8,687.9 331,688.3 	"/>
+        <polygon className="st22" points="372,533.6 419.7,534.8 424.5,687.5 378.8,687.9 	"/>
+        <polygon className="st23" points="419.7,534.8 467.2,536 476,687.5 424.5,687.5 	"/>
+        <polygon className="st24" points="467.2,536 514.6,537.2 523.6,686.6 476,687 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st25" points="18,47.2 21.3,78.9 130.7,78.9 129.8,46.5 	"/>
+        <polygon className="st26" points="130.7,78.9 21.3,78.9 25,114.5 131.6,114.5 	"/>
+        <polygon className="st27" points="131.6,114.5 25,114.5 28.8,151 132.6,151 	"/>
+        <polygon className="st28" points="132.6,151 28.8,151 32.7,188.7 133.7,188.7 	"/>
+        <polygon className="st29" points="133.7,188.7 32.7,188.7 36.9,229 134.8,229 	"/>
+        <polygon className="st30" points="134.8,229 36.9,229 41.1,269.5 135.9,269.5 	"/>
+        <polygon className="st31" points="135.9,269.5 41.1,269.5 45.5,312.4 137.1,312.4 	"/>
+        <polygon className="st32" points="137.1,312.4 45.5,312.4 50.3,358.2 138.4,358.2 	"/>
+        <polygon className="st33" points="138.4,358.2 50.3,358.2 55.2,405.6 139.7,405.6 	"/>
+        <polygon className="st34" points="139.7,405.6 55.2,405.6 60.4,455.4 141,455.4 	"/>
+        <polygon className="st35" points="141,455.4 60.4,455.4 65.8,507.5 142.5,507.5 	"/>
+        <polygon className="st36" points="142.5,507.5 65.8,507.5 71.7,564.1 144,564.1 	"/>
+        <polygon className="st37" points="144,564.1 71.7,564.1 77.9,624.3 145.7,624.3 	"/>
+        <polygon className="st38" points="145.7,624.3 77.9,624.3 84.8,690.4 147.5,689.8 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st39" points="390.9,44.9 279.3,45.6 281.1,88.1 393.6,90.8 	"/>
+        <polygon className="st40" points="393.6,90.8 281.1,88.1 282.8,128.8 396,131.6 	"/>
+        <polygon className="st41" points="396,131.6 282.8,128.8 284.6,171 398.5,173.8 	"/>
+        <polygon className="st42" points="279.3,45.6 166.1,46.3 167.1,85.3 281.1,88.1 	"/>
+        <polygon className="st43" points="281.1,88.1 167.1,85.3 168.1,126 282.8,128.8 	"/>
+        <polygon className="st44" points="282.8,128.8 168.1,126 169.1,168.2 284.6,171 	"/>
+        <polygon className="st45" points="398.5,173.8 353.8,172.7 359,280.4 393.5,281.3 404.3,271 	"/>
+        <polygon className="st46" points="353.8,172.7 307.7,171.6 312.4,279.3 359,280.4 	"/>
+        <polygon className="st47" points="307.7,171.6 261.8,170.5 266.5,278.2 312.4,279.3 	"/>
+        <polygon className="st48" points="261.8,170.5 216.4,169.4 219.1,277 266.5,278.2 	"/>
+        <polygon className="st49" points="216.4,169.4 169.1,168.2 171.5,264 184.8,276.2 219.1,277 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st50" points="561.2,71.1 687.3,73.8 691.5,123.9 564.6,121.2 	"/>
+        <polygon className="st51" points="564.6,121.2 691.5,123.9 695.7,173.4 567.9,170.7 	"/>
+        <polygon className="st52" points="431.3,44.7 559.3,43.9 562.2,86.2 433.6,83.5 	"/>
+        <polygon className="st53" points="433.6,83.5 562.2,86.2 565,128.1 436.1,125.3 	"/>
+        <polygon className="st54" points="436.1,125.3 565,128.1 567.9,170.7 438.6,167.9 	"/>
+        <polygon className="st55" points="653.4,172.5 695.7,173.4 704.7,279.3 695.5,289.4 661.3,288.5 	"/>
+        <polygon className="st56" points="610.9,171.6 653.4,172.5 661.3,288.5 618.7,287.3 	"/>
+        <polygon className="st57" points="567.9,170.7 610.9,171.6 618.7,287.3 575.8,286.2 	"/>
+        <polygon className="st58" points="524.8,169.8 567.9,170.7 575.8,286.2 532.7,285 	"/>
+        <polygon className="st59" points="480.9,168.8 524.8,169.8 532.7,285 488.7,283.8 	"/>
+        <polygon className="st60" points="438.6,167.9 480.9,168.8 488.7,283.8 456.6,282.9 444.8,272.3 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st61" points="822.2,42.3 921.2,41.7 926.6,94.3 826.8,91.9 	"/>
+        <polygon className="st62" points="826.8,91.9 926.6,94.3 931.6,143.4 831.4,141 	"/>
+        <polygon className="st63" points="831.4,141 931.6,143.4 936.8,194.1 836.2,191.6 	"/>
+        <polygon className="st64" points="727,89.5 826.8,91.9 831.4,141 731,138.5 	"/>
+        <polygon className="st65" points="731,138.5 831.4,141 836.2,191.6 735.1,189.2 	"/>
+        <polygon className="st66" points="886.8,192.9 936.8,194.1 946.2,285.2 937,296 896.4,294.9 	"/>
+        <polygon className="st67" points="836.2,191.6 886.5,192.9 896.4,294.9 845.8,293.6 	"/>
+        <polygon className="st68" points="785.6,190.4 836.2,191.6 845.8,293.6 795.2,292.2 	"/>
+        <polygon className="st69" points="735.1,189.2 785.6,190.4 795.2,292.2 757.4,291.1 742.3,278 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st70" points="1070.4,40.8 1181.2,40.1 1187.7,83 1074.8,79.8 	"/>
+        <polygon className="st71" points="1074.8,79.8 1187.7,83 1193.6,122.7 1079.4,119.5 	"/>
+        <polygon className="st72" points="1079.4,119.5 1193.6,122.7 1199.5,161.5 1083.9,158.3 	"/>
+        <polygon className="st73" points="1083.9,158.3 1199.5,161.5 1205.5,201.3 1088.4,198.1 	"/>
+        <polygon className="st74" points="959.3,41.4 1070.4,40.8 1074.8,79.8 963.4,76.7 	"/>
+        <polygon className="st75" points="963.4,76.7 1074.8,79.8 1079.4,119.5 967.9,116.4 	"/>
+        <polygon className="st76" points="967.9,116.4 1079.4,119.5 1083.9,158.3 972.3,154.8 	"/>
+        <polygon className="st77" points="972.4,155.2 1083.9,158.3 1088.4,198.1 977,194.9 	"/>
+        <polygon className="st78" points="1159.7,200.1 1205.5,201.3 1219.2,292.4 1208.1,302.1 1171.3,301.2 	"/>
+        <polygon className="st79" points="1115.2,198.8 1159.7,200.1 1171.3,301.2 1126.9,300 	"/>
+        <polygon className="st80" points="1069.5,197.5 1115.2,198.8 1126.9,300 1081.2,298.8 	"/>
+        <polygon className="st81" points="1021.9,196.2 1069.5,197.5 1081.2,298.8 1033.6,297.6 	"/>
+        <polygon className="st82" points="977,194.9 1021.9,196.2 1033.6,297.6 1000.1,296.7 987.4,286.1 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st83" points="1179.4,338.3 1214.6,339.2 1227.8,350 1238.4,429.7 1191.4,428.5 	"/>
+        <polygon className="st84" points="1191.4,428.5 1238.4,429.7 1249.1,509.9 1241,518.7 1203.3,517.5 	"/>
+        <polygon className="st85" points="1132.5,337.1 1179.4,338.3 1191.4,428.5 1143,427.2 	"/>
+        <polygon className="st86" points="1143,427.2 1191.4,428.5 1203.3,517.5 1153.3,515.9 	"/>
+        <polygon className="st87" points="1086.3,336 1132.5,337.1 1143,427.2 1096.8,426 	"/>
+        <polygon className="st88" points="1096.8,426 1143,427.2 1153.3,515.9 1107.1,514.5 	"/>
+        <polygon className="st89" points="1039.5,334.8 1086.3,336 1096.8,426 1050,424.8 	"/>
+        <polygon className="st90" points="1050,424.8 1096.8,426 1107.1,514.5 1060.3,513 	"/>
+        <polygon className="st91" points="1003.9,333.8 1039.5,334.8 1050,424.8 1003.6,423.6 994.6,344.1 	"/>
+        <polygon className="st92" points="1003.6,423.6 1050,424.8 1060.3,513 1023.3,511.8 1012.5,500.1 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st93" points="900,331.2 941,332.2 953.2,342.7 961.8,419.7 909.7,419.1 	"/>
+        <polygon className="st94" points="849.2,329.9 900,331.2 909.7,419.1 858,418.5 	"/>
+        <polygon className="st95" points="797.5,328.6 849.2,329.9 858,418.5 806.4,417.9 	"/>
+        <polygon className="st96" points="760.8,327.6 797.5,328.6 806.4,417.9 756.8,417.3 747.9,341 	"/>
+        <polygon className="st97" points="909.7,419.1 961.8,419.7 970.6,499 960.8,509.8 919.7,508.5 	"/>
+        <polygon className="st98" points="858,418.5 909.7,419.1 919.7,508.5 866.8,506.8 	"/>
+        <polygon className="st99" points="806.4,417.9 858,418.5 866.8,506.8 815,505.2 	"/>
+        <polygon className="st100" points="756.8,417.3 806.4,417.9 815,505.2 775,503.9 765.9,496.2 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st101" points="665.2,325.2 698,326 710.2,337.4 716.5,413.7 672.4,412.9 	"/>
+        <polygon className="st102" points="621.9,324.1 665.2,325.2 672.4,412.9 629.2,412.2 	"/>
+        <polygon className="st103" points="579.6,323 621.9,324.1 629.2,412.2 585.2,411.4 	"/>
+        <polygon className="st104" points="535.5,321.9 579.6,323 585.2,411.4 541.2,410.6 	"/>
+        <polygon className="st105" points="491.6,320.7 535.5,321.9 541.2,410.6 497.3,409.8 	"/>
+        <polygon className="st106" points="460.6,319.9 491.6,320.7 497.3,409.8 453,409 448.9,330.6 	"/>
+        <polygon className="st107" points="672.4,412.9 716.5,413.7 723.2,494.1 714.3,504.5 679.9,503.5 	"/>
+        <polygon className="st108" points="629.2,412.2 672.4,412.9 679.9,503.5 636.7,502.3 	"/>
+        <polygon className="st109" points="585.2,411.4 629.2,412.2 636.7,502.3 591,501.1 	"/>
+        <polygon className="st110" points="541.2,410.6 585.2,411.4 591,501.1 546.9,499.8 	"/>
+        <polygon className="st111" points="497.3,409.8 541.2,410.6 546.9,499.8 502.9,498.6 	"/>
+        <polygon className="st112" points="453,409 497.3,409.8 502.9,498.6 467.6,497.6 457.2,487.7 	"/>
+        </g>
+        <g className="opacity50">
+        <polygon className="st113" points="362.2,317.5 394.8,318.4 408.9,329.8 412.7,407.6 366.5,406.4 	"/>
+        <polygon className="st114" points="315.3,316.3 362.2,317.5 366.5,406.4 319.6,405.2 	"/>
+        <polygon className="st115" points="269,315.1 315.3,316.3 319.6,405.2 272.6,404 	"/>
+        <polygon className="st116" points="222.6,313.9 269,315.1 272.6,404 225.3,402.8 	"/>
+        <polygon className="st117" points="192,313.1 222.6,313.9 225.3,402.8 182.2,401.6 181,323.4 	"/>
+        <polygon className="st118" points="366.5,406.4 412.7,407.6 416.4,485.5 405.2,496.7 370.8,495.8 	"/>
+        <polygon className="st119" points="319.6,405.2 366.5,406.4 370.8,495.8 323.9,494.5 	"/>
+        <polygon className="st120" points="272.6,404 319.6,405.2 323.9,494.5 276.1,493.3 	"/>
+        <polygon className="st121" points="225.3,402.8 272.6,404 276.1,493.3 228,492 	"/>
+        <polygon className="st122" points="182.2,401.6 225.3,402.8 228,492 194.7,491.2 183.3,478.1 	"/>
+        </g>
+
+    </svg>
+  )
+}
+export default Planos
--- src/components/Terrenos/Terrenos.jsx
+++ src/components/Terrenos/Terrenos.jsx
@@ -1,34 +1,57 @@
-import Financiacion from "./Financiacion/Financiacion"
-import HeroTerrenos from "./HeroTerrenos/HeroTerrenos"
-import HogarSonado from "./HogarSonado/HogarSonado"
-import "./terrenos.css"
+import Financiacion from "./Financiacion/Financiacion";
+import HeroTerrenos from "./HeroTerrenos/HeroTerrenos";
+import HogarSonado from "./HogarSonado/HogarSonado";
+import Planos from "./Planos/Planos";
+import "./terrenos.css";
 const Terrenos = () => {
   return (
     <>
-      <HeroTerrenos/>
-      <HogarSonado/>
+      <HeroTerrenos />
+      <HogarSonado />
       <section className="padding">
-        <Financiacion/>
+        <Financiacion />
       </section>
       <section className="padding20 secc80">
         <h2 style={{ paddingBottom: "20px" }}>Planos disponibles</h2>
         <p>Práctico y seguro, pensado para la comodidad de tus visitas.</p>
-        <h2 style={{ padding: "20px 0" }}>Contamos con diferentes tamaños de lotes para adaptarse a tus necesidades.</h2>
+        <h2 style={{ padding: "20px 0" }}>
+          Contamos con diferentes tamaños de lotes para adaptarse a tus
+          necesidades.
+        </h2>
         <p>
           - Lotes desde 242 m² hasta 481 m²
-          <br/>
-          <br/>
+          <br />
+          <br />
           - Diferentes ubicaciones dentro del barrio
-          <br/>
-          <br/>
-          - Asesoramiento personalizado
+          <br />
+          <br />- Asesoramiento personalizado
         </p>
       </section>
       <section className="secc80">
-        <h1 style={{textAlign:"center", padding:"50px 0", paddingBottom:"20px"}}>¿Listo para empezar?</h1>
-        <h2 style={{textAlign:"center", paddingBottom:"10px", fontWeight:"normal"}}>No pierdas esta oportunidad única de ser parte de Barrio Cerrado Isla Bogado.</h2>
-    </section>
+        <h1
+          style={{
+            textAlign: "center",
+            padding: "50px 0",
+            paddingBottom: "20px",
+          }}
+        >
+          ¿Listo para empezar?
+        </h1>
+        <h2
+          style={{
+            textAlign: "center",
+            paddingBottom: "10px",
+            fontWeight: "normal",
+          }}
+        >
+          No pierdas esta oportunidad única de ser parte de Barrio Cerrado Isla
+          Bogado.
+        </h2>
+      </section>
+      <section className="secc80">
+        <Planos/>
+      </section>
     </>
-  )
-}
-export default Terrenos
+  );
+};
+export default Terrenos;
